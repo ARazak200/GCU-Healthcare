b6e6e1e88c6b00e57f720d70d2896d4d713f57eb
Aadam Razak <58515893+ARazak200@users.noreply.github.com>
2021-04-02 19:27:10 +0100

    adding front end basic

---
+++ GCUHealthcareViewsTest/GCUHealthcareViewsTest/Controllers/PersonalDataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GCUHealthcareViewsTest.Controllers
+{
+    public class PersonalDataController : Controller
+    {
+        // GET: PersonalData
+        public ActionResult Index()
+        {
+            return View();//landing page
+                          //TODO create a link to index(Home)>>personal data>>(related attribules )
+        }
+    }
+}
---
+++ GCUHealthcareViewsTest/GCUHealthcareViewsTest/Controllers/SleepController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GCUHealthcareViewsTest.Controllers
+{
+    public class SleepController : Controller
+    {
+        // GET: Sleep
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // TODO will need to update with count.cshtml
+    }
+}
