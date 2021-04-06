29de193edf4d641f0fdb53e2f7c39874734a0fe8
Aadam Razak <58515893+ARazak200@users.noreply.github.com>
2021-04-06 13:40:48 +0100

    adding in the sub pages for calories section

--- GCUHealthcareViewsTest/GCUHealthcareViewsTest/Controllers/CaloriesController.cs
+++ GCUHealthcareViewsTest/GCUHealthcareViewsTest/Controllers/CaloriesController.cs
@@ -9,14 +9,21 @@ namespace GCUHealthcareViewsTest.Controllers
     public class CaloriesController : Controller
     {
         // GET: Calories
-        public ActionResult Index()
+        public ActionResult Index()//showing the current calories 
         {
             return View();
         }
-
-        public ActionResult CAdd()
+        public ActionResult CAdd()//input the values for adding a calorie intake with date and value
+        {
+            return View();
+        } public ActionResult CDelete()//selecting a list and be able to remove from db and show a confiramtion
         {
             return View();
         }
+        public ActionResult CEdit()//selecting a list and be able to edit the selected calorie values 
+        {
+            return View();
+        }
+       
     }
 }
