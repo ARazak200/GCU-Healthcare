db14787889a9aff9aa1ebc57f2ab8b19f8ed4066
Aadam Razak <58515893+ARazak200@users.noreply.github.com>
2021-04-28 19:49:19 +0100

    adding in sleep section for personal data

--- GCUHealthcareViewsTest/GCUHealthcareViewsTest/Controllers/SleepController.cs
+++ GCUHealthcareViewsTest/GCUHealthcareViewsTest/Controllers/SleepController.cs
@@ -15,5 +15,19 @@ namespace GCUHealthcareViewsTest.Controllers
         }
 
         // TODO will need to update with count.cshtml
+        public ActionResult SAdd()//input the values for adding amount of sleep with hours.
+        {
+            return View();
+        }
+
+        public ActionResult SDelete()//selecting a list and be able to remove from db and show a confiramtion
+        {
+            return View();
+        }
+
+        public ActionResult SEdit()//selecting a list and be able to edit the selected sleep values 
+        {
+            return View();
+        }
     }
 }
